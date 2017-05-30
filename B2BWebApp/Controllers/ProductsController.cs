db44116548cda7c1bbd8bec7f641f9c8d80ed60d
Jan <jan-hermansen@hotmail.com>
2017-05-30 16:28:21 +0200

    Changed stuff

--- B2BWebApp/Controllers/ProductsController.cs
+++ B2BWebApp/Controllers/ProductsController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using B2BWebApp.Models.Product;
 using B2BWebApp.Service;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace B2BWebApp.Controllers
@@ -12,82 +8,13 @@ namespace B2BWebApp.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public async Task<ActionResult> IndexAsync()
+        public async Task<IActionResult> Index()
         {
             var service = new ProductService();
 
-            Products p = await service.GetAllProductsAsync();
+            Products response = await service.GetAllProductsAsync();
 
-            return View("index", p.products);
-        }
-
-        // GET: Products/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Products/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Products/Edit/id
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Products/Edit/id
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Products/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Products/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return View("index", response.products);
         }
     }
 }
